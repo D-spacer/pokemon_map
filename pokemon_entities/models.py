167b6b190b054eb00ad1978afaae47cbebcef45f
Dmitry.N <77688182+D-spacer@users.noreply.github.com>
2023-04-17 00:10:33 +0300

    added verbose_names

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -5,12 +5,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 class Pokemon(models.Model):
-    image = models.ImageField(blank=True, null=True, upload_to='images')
-    title_ru = models.CharField(max_length=200, blank=True)
-    title_en = models.CharField(max_length=200, blank=True)
-    title_jp = models.CharField(max_length=200, blank=True)
-    description = models.TextField(blank=True)
-    child = models.ForeignKey("self", null=True, blank=True, on_delete=models.DO_NOTHING, related_name='children')
+    image = models.ImageField(blank=True, null=True, upload_to='images', verbose_name='Изображение')
+    title_ru = models.CharField(max_length=200, blank=True, verbose_name='Русское название')
+    title_en = models.CharField(max_length=200, blank=True, verbose_name='Английское название')
+    title_jp = models.CharField(max_length=200, blank=True, verbose_name='Японское название')
+    description = models.TextField(blank=True, verbose_name='Описание')
+    child = models.ForeignKey("self", null=True, blank=True, on_delete=models.DO_NOTHING, related_name='children', , verbose_name='Предыдущая эволюция')
     objects = models.Manager()
 
     def __str__(self):
@@ -18,16 +18,16 @@ class Pokemon(models.Model):
 
 
 class PokemonEntity(models.Model):
-    lat = models.FloatField(null=True)
-    lon = models.FloatField(null=True)
+    lat = models.FloatField(null=True, verbose_name='Широта')
+    lon = models.FloatField(null=True, verbose_name='Долгота')
     pokemon = models.ForeignKey(Pokemon, on_delete=models.DO_NOTHING, related_name='entities')
-    appeared_at = models.DateTimeField(null=True)
-    disappeared_at = models.DateTimeField(null=True)
-    level = models.IntegerField(null=True)
-    health = models.IntegerField(null=True)
-    strength = models.IntegerField(null=True)
-    defence = models.IntegerField(null=True)
-    stamina = models.IntegerField(null=True)
+    appeared_at = models.DateTimeField(null=True, verbose_name='Время появления')
+    disappeared_at = models.DateTimeField(null=True, verbose_name='Время исчезновения')
+    level = models.IntegerField(null=True, verbose_name='Уровень')
+    health = models.IntegerField(null=True, verbose_name='Здоровье')
+    strength = models.IntegerField(null=True, verbose_name='Сила')
+    defence = models.IntegerField(null=True, verbose_name='Защита')
+    stamina = models.IntegerField(null=True, verbose_name='Выносливость')
     objects = models.Manager()
     
     def __str__(self):
